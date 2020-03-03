3a24801a03b3be49a014814505797194bfe18737
jtan-devicebits <jtan@devicebits.comm>
2020-03-04 04:53:52 +0800

    Add JS

--- comcastar.js
+++ comcastar.js
@@ -3,3 +3,12 @@ function SetVariable(variable, value) {
     console.log(variable + ' : ' + value)
     variables[variable] = value
 }
+
+function GenerateNotes() {
+    var notes = "NOTES:\n"
+    for (const [key, value] of Object.entries(variables)) {
+        console.log(key, value);
+        notes = notes + key + ' : ' + value + '\n'
+    }
+    document.getElementById("generated_codes").innerHTML = notes;
+}
