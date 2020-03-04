b61f9d38a03be17039a7661e647979efe7aa763d
jtan-devicebits <jtan@devicebits.comm>
2020-03-04 22:41:30 +0800

    JS Code Update

--- comcastar.js
+++ comcastar.js
@@ -1,7 +1,4 @@
 function clearVariable(variable, value) {
-    // console.log("clearVariable function..")
-    // localStorage.clear()
-    // console.log("Local Storage Clear..")
     local_variables = {}
     localStorage.setItem('local_variables', JSON.stringify(local_variables))
     console.log("Emptied Local Variable")
@@ -17,14 +14,15 @@ function SetVariable(variable, value) {
     localStorage.setItem('local_variables', JSON.stringify(local_variables))
 }
 
-// function GenerateNotes() {
-//     var notes = "NOTES:\n\n"
-//     for (const [key, value] of Object.entries(variables)) {
-//         console.log(key, value);
-//         notes = notes + key + ' : ' + value + '\n'
-//     }
-//     document.getElementById("generated_codes").innerHTML = notes;
-// }
+function GenerateNotes() {
+    var local_variables = JSON.parse(localStorage.getItem('local_variables'))
+    var notes = "NOTES:\n\n"
+    for (const [key, value] of Object.entries(local_variables)) {
+        console.log(key, value);
+        notes = notes + key + ' : ' + value + '\n'
+    }
+    document.getElementById("generated_codes").innerHTML = notes;
+}
 
 // var generated_codes_exist = document.getElementById("generated_codes_exist");
 // if (typeof(generated_codes_exist) != 'undefined' && generated_codes_exist != null) {
