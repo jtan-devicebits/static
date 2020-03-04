ea3de6beb844c3bbbb4960111bf6dfae4ecf02fd
jtan-devicebits <jtan@devicebits.comm>
2020-03-04 22:22:02 +0800

    JS Code Update

--- comcastar.js
+++ comcastar.js
@@ -1,19 +1,29 @@
 variables = {}
+
+function clearVariable(variable, value) {
+    localStorage.clear()
+    local_variables = {}
+    local_variables[variable] = value
+    localStorage.setItem('local_variables', JSON.stringify(local_variables))
+}
+
 function SetVariable(variable, value) {
     console.log(variable + ' : ' + value)
-    variables[variable] = value
+    var local_variables = JSON.parse(localStorage.getItem('local_variables'))
+    local_variables[variable] = value
+    localStorage.setItem('local_variables', JSON.stringify(local_variables))
 }
 
-function GenerateNotes() {
-    var notes = "NOTES:\n\n"
-    for (const [key, value] of Object.entries(variables)) {
-        console.log(key, value);
-        notes = notes + key + ' : ' + value + '\n'
-    }
-    document.getElementById("generated_codes").innerHTML = notes;
-}
+// function GenerateNotes() {
+//     var notes = "NOTES:\n\n"
+//     for (const [key, value] of Object.entries(variables)) {
+//         console.log(key, value);
+//         notes = notes + key + ' : ' + value + '\n'
+//     }
+//     document.getElementById("generated_codes").innerHTML = notes;
+// }
 
-var generated_codes_exist = document.getElementById("generated_codes_exist");
-if (typeof(generated_codes_exist) != 'undefined' && generated_codes_exist != null) {
-    GenerateNotes()
-}
+// var generated_codes_exist = document.getElementById("generated_codes_exist");
+// if (typeof(generated_codes_exist) != 'undefined' && generated_codes_exist != null) {
+//     GenerateNotes()
+// }
