639debf90dac2d1d0e7dc0fab22722f0fe8a2e00
jtan-devicebits <jtan@devicebits.comm>
2020-03-11 00:23:26 +0800

    Foxtel

--- foxtel.js
+++ foxtel.js
@@ -1,9 +1,21 @@
 function Zoom() {
   var yourImg = document.getElementById('myImg');
+  var zoom = 0;
+
   if(yourImg && yourImg.style) {
-      yourImg.style.height = '100%';
-      yourImg.style.width = '100%';
-      yourImg.style.maxWidth = '100%';
+
+      if (zoom === 0) {
+        yourImg.style.height = '100%';
+        yourImg.style.width = '100%';
+        yourImg.style.maxWidth = '100%';
+        zoom = 1;
+      } else {
+        yourImg.style.height = '';
+        yourImg.style.width = '';
+        yourImg.style.maxWidth = '';
+        zoom = 0;
+      }
+
   }
 }
 
