508553f2009695b96514034ef7df5639ef6f26a5
jtan-devicebits <jtan@devicebits.comm>
2020-03-11 00:54:24 +0800

    Foxtel

--- foxtel.js
+++ foxtel.js
@@ -1,20 +1,20 @@
-var zoom = 0;
+var zoom = {}
 
 function Zoom(imageID) {
   var yourImg = document.getElementById(imageID);
 
   if(yourImg && yourImg.style) {
 
-      if (zoom === 0) {
+      if (zoom[imageID]===0) {
         yourImg.style.height = '100%';
         yourImg.style.width = '100%';
         yourImg.style.maxWidth = '100%';
-        zoom = 1;
+        zoom[imageID] = 1;
       } else {
         yourImg.style.height = '';
         yourImg.style.width = '';
         yourImg.style.maxWidth = '';
-        zoom = 0;
+        zoom[imageID] = 0;
       }
 
   }
